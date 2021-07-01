9c81de39e271ac65440c95593b91b1b0b75c1443
evelingcastillon <evelingcastillon0304@gmail.com>
2021-07-01 16:44:18 +0200

    add in html

--- index.php
+++ index.php
@@ -44,3 +44,9 @@ class Phone extends Product
         $this->operatingSystem = $operatingSystem;
     }
 }
+
+$cellulari = [
+    new Phone('Iphone 12', "Penta Band - 5G - Wi-Fi - NFC - A-GPS Ceramic Shield - Display Super Retina XDR 6,1''. Resistente a gocce e schizzi - Modalita' Notte Doppia Fotocamera posteriore 12 MP - iOS 14 Processore A14 Bionic - Memoria interna: 128 GB, distribuito da APPLE Italia", 849.00, 1, 128, 'iOS 14'),
+    new Phone(' Galaxy S21', 'La foto perfetta non potrà più sfuggirti. Dai il benvenuto a Galaxy S21 5G e S21+ 5G. Grazie a una risoluzione 8K di livello cinematografico, potrai ottenere foto staordinarie direttamente da un video. Qualcosa di rivoluzionario, per entrambi i mondi.', 1079.00, 1, 128, 'Android'),
+    
+];
